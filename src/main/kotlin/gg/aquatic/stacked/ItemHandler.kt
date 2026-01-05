d1cff7796544f2fa6c21445e2e6888a28e3867d8
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-05 19:54:06 +0100

    Refactor `ItemHandler` interaction logic into dedicated handler methods

--- src/main/kotlin/gg/aquatic/stacked/ItemHandler.kt
+++ src/main/kotlin/gg/aquatic/stacked/ItemHandler.kt
@@ -19,7 +19,6 @@ object ItemHandler {
         NamespacedKey(Stacked.plugin, "Custom_Item_Registry")
     }
     val listenInteractions = mutableMapOf<String, (StackedItemInteractEvent) -> Unit>()
-    private val configItems = HashSet<String>()
 
     val eventBus by lazy {
         eventBusBuilder {
@@ -30,80 +29,15 @@ object ItemHandler {
 
     fun initialize() {
         event<PlayerInteractEvent> {
-            if (it.hand == EquipmentSlot.OFF_HAND) return@event
-            if (listenInteractions.isEmpty()) return@event
-            val item = it.item ?: return@event
-            val aitem = item.toStacked() ?: return@event
-            val registry = aitem.registryId() ?: return@event
-
-            val interaction = listenInteractions[registry] ?: return@event
-
-            val interactType = when (it.action) {
-                Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK -> if (it.player.isSneaking) StackedItemInteractEvent.InteractType.SHIFT_RIGHT else StackedItemInteractEvent.InteractType.RIGHT
-                Action.LEFT_CLICK_AIR, Action.LEFT_CLICK_BLOCK -> if (it.player.isSneaking) StackedItemInteractEvent.InteractType.SHIFT_LEFT else StackedItemInteractEvent.InteractType.LEFT
-                else -> return@event
-            }
-
-            val aitemEvent = StackedItemInteractEvent(
-                it.player, aitem, item, it, interactType,
-            )
-            interaction(aitemEvent)
-            eventBus.post(aitemEvent)
+            handleInteract(it)
         }
+
         event<PlayerSwapHandItemsEvent> {
-            val item = it.mainHandItem
-            val aitem = item.toStacked() ?: return@event
-            val registry = aitem.registryId() ?: return@event
-            val interaction = listenInteractions[registry] ?: return@event
-
-            val interactType =
-                if (it.player.isSneaking) StackedItemInteractEvent.InteractType.SHIFT_SWAP else StackedItemInteractEvent.InteractType.SWAP
-            val aitemEvent = StackedItemInteractEvent(
-                it.player, aitem, item, it, interactType,
-            )
-            interaction(aitemEvent)
-            eventBus.post(aitemEvent)
+            handleSwapHandItems(it)
         }
+
         event<InventoryClickEvent> {
-            val player = it.whoClicked as? Player ?: return@event
-            val item = it.currentItem ?: return@event
-            val aitem = item.toStacked() ?: return@event
-            val registry = aitem.registryId() ?: return@event
-            val interaction = listenInteractions[registry] ?: return@event
-
-            val interactType = when (it.click) {
-                ClickType.SHIFT_LEFT -> StackedItemInteractEvent.InteractType.INVENTORY_SHIFT_LEFT
-                ClickType.LEFT -> StackedItemInteractEvent.InteractType.INVENTORY_LEFT
-                ClickType.SHIFT_RIGHT -> StackedItemInteractEvent.InteractType.INVENTORY_SHIFT_RIGHT
-                ClickType.RIGHT -> StackedItemInteractEvent.InteractType.INVENTORY_RIGHT
-                ClickType.NUMBER_KEY -> {
-                    when (it.hotbarButton) {
-                        1 -> StackedItemInteractEvent.InteractType.NUM_1
-                        2 -> StackedItemInteractEvent.InteractType.NUM_2
-                        3 -> StackedItemInteractEvent.InteractType.NUM_3
-                        4 -> StackedItemInteractEvent.InteractType.NUM_4
-                        5 -> StackedItemInteractEvent.InteractType.NUM_5
-                        6 -> StackedItemInteractEvent.InteractType.NUM_6
-                        7 -> StackedItemInteractEvent.InteractType.NUM_7
-                        8 -> StackedItemInteractEvent.InteractType.NUM_8
-                        9 -> StackedItemInteractEvent.InteractType.NUM_9
-                        else -> StackedItemInteractEvent.InteractType.NUM_0
-                    }
-                }
-
-                ClickType.DROP -> StackedItemInteractEvent.InteractType.INVENTORY_DROP
-                ClickType.SWAP_OFFHAND -> StackedItemInteractEvent.InteractType.INVENTORY_SWAP
-                else -> return@event
-            }
-            val aitemEvent = StackedItemInteractEvent(
-                player,
-                aitem,
-                item,
-                it,
-                interactType,
-            )
-            interaction(aitemEvent)
-            eventBus.post(aitemEvent)
+            handleInventoryClick(it)
         }
     }
 
@@ -119,6 +53,85 @@ object ItemHandler {
             item,
             options
         )
+    }
+
+    private fun handleInteract(event: PlayerInteractEvent) {
+        if (event.hand == EquipmentSlot.OFF_HAND) return
+        if (listenInteractions.isEmpty()) return
+        val item = event.item ?: return
+        val aitem = item.toStacked() ?: return
+        val registry = aitem.registryId() ?: return
+
+        val interaction = listenInteractions[registry] ?: return
+
+        val interactType = when (event.action) {
+            Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK -> if (event.player.isSneaking) StackedItemInteractEvent.InteractType.SHIFT_RIGHT else StackedItemInteractEvent.InteractType.RIGHT
+            Action.LEFT_CLICK_AIR, Action.LEFT_CLICK_BLOCK -> if (event.player.isSneaking) StackedItemInteractEvent.InteractType.SHIFT_LEFT else StackedItemInteractEvent.InteractType.LEFT
+            else -> return
+        }
+
+        val aitemEvent = StackedItemInteractEvent(
+            event.player, aitem, item, event, interactType,
+        )
+        interaction(aitemEvent)
+        eventBus.post(aitemEvent)
+    }
+
+    private fun handleSwapHandItems(event: PlayerSwapHandItemsEvent) {
+        val item = event.mainHandItem
+        val aitem = item.toStacked() ?: return
+        val registry = aitem.registryId() ?: return
+        val interaction = listenInteractions[registry] ?: return
+
+        val interactType =
+            if (event.player.isSneaking) StackedItemInteractEvent.InteractType.SHIFT_SWAP else StackedItemInteractEvent.InteractType.SWAP
+        val aitemEvent = StackedItemInteractEvent(
+            event.player, aitem, item, event, interactType,
+        )
+        interaction(aitemEvent)
+        eventBus.post(aitemEvent)
+    }
+
+    private fun handleInventoryClick(event: InventoryClickEvent) {
+        val player = event.whoClicked as? Player ?: return
+        val item = event.currentItem ?: return
+        val aitem = item.toStacked() ?: return
+        val registry = aitem.registryId() ?: return
+        val interaction = listenInteractions[registry] ?: return
+
+        val interactType = when (event.click) {
+            ClickType.SHIFT_LEFT -> StackedItemInteractEvent.InteractType.INVENTORY_SHIFT_LEFT
+            ClickType.LEFT -> StackedItemInteractEvent.InteractType.INVENTORY_LEFT
+            ClickType.SHIFT_RIGHT -> StackedItemInteractEvent.InteractType.INVENTORY_SHIFT_RIGHT
+            ClickType.RIGHT -> StackedItemInteractEvent.InteractType.INVENTORY_RIGHT
+            ClickType.NUMBER_KEY -> {
+                when (event.hotbarButton) {
+                    1 -> StackedItemInteractEvent.InteractType.NUM_1
+                    2 -> StackedItemInteractEvent.InteractType.NUM_2
+                    3 -> StackedItemInteractEvent.InteractType.NUM_3
+                    4 -> StackedItemInteractEvent.InteractType.NUM_4
+                    5 -> StackedItemInteractEvent.InteractType.NUM_5
+                    6 -> StackedItemInteractEvent.InteractType.NUM_6
+                    7 -> StackedItemInteractEvent.InteractType.NUM_7
+                    8 -> StackedItemInteractEvent.InteractType.NUM_8
+                    9 -> StackedItemInteractEvent.InteractType.NUM_9
+                    else -> StackedItemInteractEvent.InteractType.NUM_0
+                }
+            }
+
+            ClickType.DROP -> StackedItemInteractEvent.InteractType.INVENTORY_DROP
+            ClickType.SWAP_OFFHAND -> StackedItemInteractEvent.InteractType.INVENTORY_SWAP
+            else -> return
+        }
+        val aitemEvent = StackedItemInteractEvent(
+            player,
+            aitem,
+            item,
+            event,
+            interactType,
+        )
+        interaction(aitemEvent)
+        eventBus.post(aitemEvent)
     }
 
     interface Factory {
