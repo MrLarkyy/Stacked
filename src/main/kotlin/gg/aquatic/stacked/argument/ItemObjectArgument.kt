00b1a1fd3c7cc7357af24a20d40362b5096e13f1
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-21 10:33:29 +0100

    Refactor argument classes into their respective modules
    Moved `BlockArgument` to the `Blokk` module and `ItemObjectArgument` to the `Stacked` module for improved modularity and separation of concerns. Updated all relevant imports in the project accordingly.

---
+++ src/main/kotlin/gg/aquatic/stacked/argument/ItemObjectArgument.kt
@@ -0,0 +1,28 @@
+package gg.aquatic.stacked.argument
+
+import gg.aquatic.common.argument.ObjectArgument
+import gg.aquatic.common.argument.ObjectArgumentFactory
+import gg.aquatic.stacked.StackedItem
+import org.bukkit.configuration.ConfigurationSection
+
+class ItemObjectArgument(
+    id: String,
+    defaultValue: StackedItem?,
+    required: Boolean,
+    aliases: Collection<String> = listOf(),
+) : ObjectArgument<StackedItem>(
+    id, defaultValue,
+    required,
+    aliases
+) {
+    override val serializer: ObjectArgumentFactory<StackedItem?>
+        get() {
+            return Serializer
+        }
+
+    object Serializer : ObjectArgumentFactory<StackedItem?>() {
+        override fun load(section: ConfigurationSection, id: String): StackedItem? {
+            return StackedItem.Companion.loadFromYml(section.getConfigurationSection(id) ?: return null)
+        }
+    }
+}
