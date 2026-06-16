d2d32e5127dffa9ab05c0781e42d8aa292d7c8a6
MrLarkyy_ <studiotadikl@gmail.com>
2026-06-17 00:33:29 +0200

    Update Foojay toolchain resolver configuration in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,15 @@
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.gradle.toolchains.foojay-resolver") version "1.0.0"
 }
+
+toolchainManagement {
+    jvm {
+        javaRepositories {
+            repository("foojay") {
+                resolverClass = org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java
+            }
+        }
+    }
+}
+
 rootProject.name = "Stacked"
